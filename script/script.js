0f78b1c334e7d1d2cdbc341f8137e29e02394f10
Yash Narkhedkar <yashnarkhedkar@gmail.com>
2022-07-26 17:12:14 +0530

    script updation

--- script/script.js
+++ script/script.js
@@ -1,59 +1,27 @@
 // 🌟 APP: Tip Calculator
 
-// Get global access to all inputs / divs here (you'll need them later 😘)
-// bill input, tip input, number of people div, and per person total div
-let billInput = document.getElementById('billTotalInput');
-let tipInput = document.getElementById('tip_input');
-let numberOfPeople = document.getElementById('numberOfPeople');
-let perPersonTotal = document.getElementById('perPersonTotal');
-
-
-// ** Calculate the total bill per person **
 const calculateBill = () => {
-    // get bill from user input & convert it into a number
-    let input = Number(document.getElementById('billTotalInput').value);
-  
-    // get the tip from user & convert it into a percentage (divide by 100)
-    let tip = Number(document.getElementById('tip_input').value);
-    console.log(tip);
-    // get the total tip amount
-    
-  
-    // calculate the total (tip amount + bill)
-    
-  
-    // calculate the per person total (total divided by number of people)
-  
-  
-    // update the perPersonTotal on DOM & show it to user
-  
+  let billInput = Number(document.getElementById('billTotalInput').value);
+  let tipInput = Number(document.getElementById('tipInput').value) / 100;
+  let numberOfPeople = document.getElementById('numberOfPeople').textContent;
+  let perPersonTotal = document.querySelector('#perPersonTotal');
+
+  let totalamount = (((billInput * tipInput) + billInput) / numberOfPeople).toFixed(2);
+  perPersonTotal.innerHTML = `<b>₹ ${totalamount}</b>`;
+}
+
+const increasePeople = () => {
+  let numberOfPeople = document.getElementById('numberOfPeople').textContent;
+  numberOfPeople++;
+  document.querySelector('#numberOfPeople').textContent = numberOfPeople;
+  calculateBill()
+}
+
+const decreasePeople = () => {
+  let numberOfPeople = document.getElementById('numberOfPeople').textContent;
+  if (numberOfPeople > 1) {
+    numberOfPeople--;
+    document.querySelector('#numberOfPeople').textContent = numberOfPeople;
+    calculateBill()
   }
-  
-  // ** Splits the bill between more people **
-  const increasePeople = () => {
-    // increment the amount of people
-  
-  
-    // update the DOM with the new number of people
-  
-  
-    // calculate the bill based on the new number of people
-  
-  }
-  
-  // ** Splits the bill between fewer people **
-  const decreasePeople = () => {
-    // guard clause
-    // if amount is 1 or less simply return
-    // (a.k.a you can't decrease the number of people to 0 or negative!)
-  
-    
-    // decrement the amount of people
-  
-  
-    // update the DOM with the new number of people
-  
-  
-    // calculate the bill based on the new number of people
-  
-  }
+}
